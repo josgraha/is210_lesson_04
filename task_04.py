ba84bb5e31785602080346c85d086ab1753a0daa
Joseph Graham <josgraha@gmail.com>
2014-09-21 22:40:44 -0400

    task4 checkpoint

--- task_04.py
+++ task_04.py
@@ -1,3 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """task_04.py: Homework 4 Task 4."""
+from data import TRANSACTIONS
+
+TOTAL = 0.0
+MINIMUM = 0.0
+MAXIMUM = 0.0
+
+for daily in TRANSACTIONS:
+    dailyTotal = 0.0
+    for trans in daily:
+        dailyTotal += trans
+        TOTAL += trans
+    if dailyTotal <= MINIMUM:
+        MINIMUM = dailyTotal
+    elif MINIMUM == 0:
+        MINIMUM = dailyTotal
+    if dailyTotal > MAXIMUM:
+        MAXIMUM = dailyTotal
+
+print '''
+
+STATISTICS
+--------------------------------------------------
+
+TOTAL:        {}
+MINIMUM:      {}
+MAXIMUM:      {}
+'''.format(TOTAL,
+           MINIMUM,
+           MAXIMUM)
--- task_05.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""task_05.py: Homework 4 Task 5."""
-
